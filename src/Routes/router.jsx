0e3c5f71bb5e8d00882744b2cd31538c7e77cc24
fd-Martin <fdmosharof@gmail.com>
2026-02-15 21:28:36 +0600

    route set

---
+++ src/Routes/router.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import AuthLayout from "../Layouts/AuthLayout";
+import HomeLayout from "../Layouts/HomeLayout";
+import DashboardLayout from "../Layouts/DashboardLayout";
+
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomeLayout,
+    
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "all-books",
+        Component: AllBooks,
+      },
+      {
+        path: "book-details/:id",
+        element: (
+          <PrivateRoute>
+            <BookDetails></BookDetails>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "*",
+        
+      },
+    ],
+  },
+  {
+    path: "auth",
+    Component: AuthLayout,
+    
+    children: [
+      {
+        path: "register",
+        Component: Register,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "*",
+        
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
+    
+    children: [
+      {
+        path: "my-profile",
+        Component: MyProfile,
+      },
+      {
+        path: "my-orders",
+        Component: MyOrders,
+      },
+      {
+        path: "invoices",
+        Component: Invoices,
+      },
+      {
+        path: "add-book",
+        element: (
+          <LibrarianRoute>
+            <AddBook></AddBook>
+          </LibrarianRoute>
+        ),
+      },
+      {
+        path: "my-books",
+        element: (
+          <LibrarianRoute>
+            <MyBooks></MyBooks>
+          </LibrarianRoute>
+        ),
+      },
+      {
+        path: "edit-book/:id",
+        element: (
+          <LibrarianRoute>
+            <BookEdited></BookEdited>
+          </LibrarianRoute>
+        ),
+      },
+      {
+        path: "orders",
+        element: (
+          <LibrarianRoute>
+            <Orders></Orders>
+          </LibrarianRoute>
+        ),
+      },
+      {
+        path: "all-users",
+        element: (
+          <AdminRoute>
+            <AllUsers></AllUsers>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-books",
+        element: (
+          <AdminRoute>
+            <ManageBooks></ManageBooks>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "payment-success",
+        Component: PaymentSuccess,
+      },
+      {
+        path: "payment-cancelled",
+        Component: PaymentCancelled,
+      },
+      {
+        path: "wishlist",
+        Component: WishList,
+      },
+      {
+        path: "*",
+        
+      },
+    ],
+  },
+]);
+
+export default Router;
